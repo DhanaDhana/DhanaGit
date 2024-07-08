4e95e21971252aa3a78dfe53b1587acf8fb37e88
DhanaRandstad <polimera.dhanakondareddy@randstaddigital.com>
2024-07-08 16:34:43 +0530

    Dhana commit2

--- djangoprojects/Register_Login_Project/Register_Login_Project/settings.py
+++ djangoprojects/Register_Login_Project/Register_Login_Project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4dtlkprr8(=6^lg$of2k-zt@d1aq30@++rfq4-in9+b(!)_-%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Register_Login_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'TEMPLATES')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
